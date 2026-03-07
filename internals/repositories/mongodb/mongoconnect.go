e90ac0f31d4637ff1e1cf8ba4e22d1ef007ed906
raman <priyanshu00268@gmail.com>
2026-03-07 15:39:53 +0530

    added error logger

--- internals/repositories/mongodb/mongoconnect.go
+++ internals/repositories/mongodb/mongoconnect.go
@@ -2,23 +2,24 @@ package mongodb
 
 import (
 	"context"
-	"fmt"
 	"log"
+
+	"github.com/devpriyanshu01/grpc_api_project_school/pkg/utils"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func CreateMongoClient(ctx context.Context) (*mongo.Client, error) {
-	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("username:password@mongodb://localhost:27017"))	
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))	
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("username:password@mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		log.Println("Error connecting to MongoDB.", err)
+		utils.ErrorHandler(err, "Failed to connect to mongodb database.")
 		return nil, err
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		fmt.Println(err)
+		utils.ErrorHandler(err, "Error while pinging mongodb.")
 		return nil, err
 	}
 
